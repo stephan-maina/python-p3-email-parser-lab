66a6737ff6129d6738772204a1d5dc3c7fd5fbed
Mr.Perfect 092 <135037455+stephan-maina@users.noreply.github.com>
2023-09-05 13:56:26 +0300

    Completed

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,15 @@
 # your code goes here!
+import re
+
+class EmailAddressParser:
+    def __init__(self, addresses_string):
+        self.addresses_string = addresses_string
+
+    def parse(self):
+        # Updated regular expression pattern for email addresses
+        email_pattern = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,7}\b'
+
+        extracted_addresses = re.findall(email_pattern, self.addresses_string, re.IGNORECASE)
+
+        unique_addresses = sorted(set(extracted_addresses))
+        return unique_addresses
